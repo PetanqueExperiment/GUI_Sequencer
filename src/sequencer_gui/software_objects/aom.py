9d8d282f401695ae0366c0f9a223742f72d40dd8
Petanque Control PC <petanqueexperiment@gmail.com>
2026-04-27 17:56:17 +0900

    put the hold signal as the default for AOM objects

--- src/sequencer_gui/software_objects/aom.py
+++ src/sequencer_gui/software_objects/aom.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+from sequencer_gui.domain.analog_stored import HOLD_SIGNAL
 from sequencer_gui.software_objects.types import AnalogParameterSpec
 
 AOM_ID = "aom"
@@ -18,7 +19,7 @@ class AomObject:
             AnalogParameterSpec(
                 param_id="amplitude",
                 label="Amplitude",
-                default=0.0,
+                default=HOLD_SIGNAL,
                 minimum=0.0,
                 maximum=1.0,
                 decimals=2,
@@ -27,7 +28,7 @@ class AomObject:
             AnalogParameterSpec(
                 param_id="frequency",
                 label="Frequency (MHz)",
-                default=80.0,
+                default=HOLD_SIGNAL,
                 minimum=0.0,
                 maximum=500.0,
                 decimals=1,
